e4d6f1bef4f194fd96ad7789300022136775048f
Ritesh Maurya <119353953+riteshmaurya089@users.noreply.github.com>
2024-03-28 09:42:28 +0530

    Update axios.js

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // import { API_URL } from '../../config';
 const instance = axios.create({
-    baseURL: "https://schoolbackend-1.onrender.com/"
+    baseURL: "https://schoolbackend-q0tw.onrender.com/"
 })
 export default instance;    
